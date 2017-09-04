8fd5337a3b33b7ba26c2cb581986e4a0d5fbcaef
TobiasGleissner <gleissner.tobias@googlemail.com>
2017-09-04 09:43:45 +0200

    added some skeleton

--- editor/src/main/java/gui/Listener.java
+++ editor/src/main/java/gui/Listener.java
@@ -1,11 +1,11 @@
-package thfedit;
+package gui;
 
 import javafx.collections.ObservableList;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ListChangeListener.Change;
 import java.lang.CharSequence;
 
-public class listener implements ListChangeListener<CharSequence>
+public class Listener implements ListChangeListener<CharSequence>
 {
     @Override
     public void onChanged(Change<? extends CharSequence> c)
