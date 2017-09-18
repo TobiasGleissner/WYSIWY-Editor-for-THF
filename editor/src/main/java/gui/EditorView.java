2ef638613d655dcb1107c85783427b0b7e670ef6
TobiasGleissner <gleissner.tobias@googlemail.com>
2017-09-18 10:49:45 +0200

    fixed fxml loader

--- editor/src/main/java/gui/EditorView.java
+++ editor/src/main/java/gui/EditorView.java
@@ -9,7 +9,7 @@ public class EditorView extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/editor.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/editor.fxml"));
         loader.setControllerFactory(t->new EditorController(new EditorModel()));
         stage.setScene(new Scene(loader.load()));
         stage.show();
