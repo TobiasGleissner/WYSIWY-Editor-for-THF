1594b0d37317ffb12a8f69a34244c3761f88882e
Irina Makarenko <irina.makarenko@fu-berlin.de>
2017-09-21 16:19:27 +0200

    adds prover lists to the menus

--- editor/src/main/java/gui/EditorController.java
+++ editor/src/main/java/gui/EditorController.java
@@ -2,15 +2,17 @@ package gui;
 
 import java.net.URL;
 
+import java.io.IOException;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import java.net.URISyntaxException;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.ResourceBundle;
+import java.util.List;
+import java.util.Iterator;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -21,11 +23,19 @@ import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import javafx.stage.FileChooser;
+
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.RadioMenuItemBuilder;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.ToggleGroup;
 
 import org.fxmisc.richtext.CodeArea;
 import org.fxmisc.richtext.LineNumberFactory;
@@ -38,13 +48,19 @@ import org.fxmisc.richtext.model.StyleSpan;
 import org.fxmisc.richtext.model.Paragraph;
 
 import gui.fileBrowser.FileTreeView;
-
 import parser.ParseContext;
+import prover.TPTPDefinitions;
+import prover.remote.HttpProver;
 
 public class EditorController implements Initializable {
+
     private EditorModel model;
     private Stage mainStage;
 
+    @FXML
+    private Menu menubarRunProver;
+    @FXML
+    private MenuButton toolbarRunProver;
     @FXML
     private CodeArea thfArea;
     @FXML
@@ -109,6 +125,13 @@ public class EditorController implements Initializable {
         this.model.wysArea = wysArea;
 
         model.updateStyle();
+
+        addCurrentlyAvailableProversToMenus();
+    }
+
+    @FXML
+    private void onNAMEExit(ActionEvent e) {
+        System.exit(0);
     }
 
     @FXML
@@ -149,13 +172,7 @@ public class EditorController implements Initializable {
     }
 
     @FXML
-    private void onFileExit(ActionEvent e) {
-        System.exit(0);
-    }
-
-    @FXML
-    private void onTestPref(ActionEvent e)
-    {
+    private void onTestPref(ActionEvent e) {
         if(Config.getFont().equals("monospace"))
             Config.setFont("xos4 Terminus");
         else
@@ -165,20 +182,17 @@ public class EditorController implements Initializable {
     }
 
     @FXML
-    private void onReparse(ActionEvent e)
-    {
+    private void onReparse(ActionEvent e) {
         model.reparse();
     }
 
     @FXML
-    private void onPrintTree(ActionEvent e)
-    {
+    private void onPrintTree(ActionEvent e) {
         model.printTPTPTrees();
     }
 
     @FXML
-    private void onTHFTextChange(PlainTextChange change)
-    {
+    private void onTHFTextChange(PlainTextChange change) {
         if(change.getInserted().equals(change.getRemoved()))
             return;
 
@@ -189,8 +203,37 @@ public class EditorController implements Initializable {
     }
 
     @FXML
-    private void onWYSTextChange(RichTextChange<Collection<String>,StyledText<Collection<String>>,Collection<String>> change)
-    {
+    private void onWYSTextChange(RichTextChange<Collection<String>,StyledText<Collection<String>>,Collection<String>> change) {
         System.out.println("wysiwyg change");
+    }
+
+    private void addCurrentlyAvailableProversToMenus() {
+        try {
+            List<String> availableProvers = HttpProver.getInstance().getAvailableProvers(TPTPDefinitions.TPTPDialect.THF);
+
+            // add list of provers to menubar
+            ToggleGroup menubarProvers = new ToggleGroup();
+            for (Iterator<String> i = availableProvers.iterator(); i.hasNext();) {
+                RadioMenuItem item = RadioMenuItemBuilder.create().toggleGroup(menubarProvers).text(i.next().replace("---"," ")).build();
+                menubarRunProver.getItems().add(item);
+            }
+
+            // add list of provers to toolbar
+            toolbarRunProver.showingProperty().addListener(new ChangeListener<Boolean>() {
+                @Override
+                public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                    if(newValue) {
+                        toolbarRunProver.getItems().clear();
+                        ToggleGroup toolbarProvers = new ToggleGroup();
+                        for (Iterator<String> i = availableProvers.iterator(); i.hasNext();) {
+                            RadioMenuItem item = RadioMenuItemBuilder.create().toggleGroup(toolbarProvers).text(i.next().replace("---"," ")).build();
+                            toolbarRunProver.getItems().add(item);
+                        }
+                    }
+                }
+            });
+        } catch (IOException e) {
+            // TODO: write log entry
+        }
     }
 }
