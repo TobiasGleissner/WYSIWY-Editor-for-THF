f12fe9cfe6c2055a597c36ac99c35d71aec278f9
arminweber <arweber@zedat.fu-berlin.de>
2017-09-26 15:33:09 +0200

    Make SpanElements comparable

--- editor/src/main/java/util/SpanElement.java
+++ editor/src/main/java/util/SpanElement.java
@@ -1,6 +1,8 @@
 package util;
 
-public class SpanElement {
+import java.util.Comparator;
+
+public class SpanElement implements Comparator<SpanElement>, Comparable<SpanElement> {
     
     int startIndex;
     int endIndex;
@@ -22,4 +24,33 @@ public class SpanElement {
         return tag;
     }
 
+    @Override
+    public int compare(SpanElement arg0, SpanElement arg1) {
+        if (arg0.getStartIndex() != arg1.getStartIndex()) 
+            return arg0.getStartIndex() - arg1.getStartIndex();
+        else if (arg0.getEndIndex() != arg1.getEndIndex())
+            return -(arg0.getEndIndex() - arg1.getEndIndex());
+        else
+            return arg0.getTag().compareTo(arg1.getTag());
+    }
+    
+    public int compareOnlyRange(SpanElement arg0, SpanElement arg1) {
+        if (arg0.getStartIndex() != arg1.getStartIndex()) 
+            return arg0.getStartIndex() - arg1.getStartIndex();
+        else 
+            return -(arg0.getEndIndex() - arg1.getEndIndex());
+    }
+
+    @Override
+    public int compareTo(SpanElement arg0) {
+        return compare(this, arg0);
+    }
+    
+    public int compareRangeTo(SpanElement arg0) {
+        return compareOnlyRange(this, arg0);
+    }
+    
+    public String toString() {
+        return "["+tag+", "+startIndex+", "+endIndex+"]";
+    }
 }
