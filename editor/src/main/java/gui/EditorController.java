0bbf3efa459e3b112b1a3c8c3000d9bed83944c7
arminweber <arweber@zedat.fu-berlin.de>
2017-09-29 12:53:53 +0200

    Copy file in file browser: replace file now possible

--- editor/src/main/java/gui/EditorController.java
+++ editor/src/main/java/gui/EditorController.java
@@ -537,14 +537,14 @@ public class EditorController implements Initializable {
                 for (File file : files) {
                     try {
                         File destination = new File (f.toPath().resolve(file.getName()).toString());
-                        while (destination.exists() && !destination.isDirectory()) {
+                        if (destination.exists() && !destination.isDirectory()) {
                             String error = "";
                             String name = file.getName();
                             while (true) {
                                 TextInputDialog dialog = new TextInputDialog(name);
                                 dialog.setTitle("Copy file");
-                                dialog.setHeaderText("Copy file"+error);
-                                dialog.setContentText("The file "+name+" already exists. Please enter a new file name:");
+                                dialog.setHeaderText("Copy file:"+error);
+                                dialog.setContentText("The file "+name+" already exists. Please enter a new file name or replace the existing file:");
                                 
                                 Optional<String> result = dialog.showAndWait();
                                 if (!result.isPresent())
@@ -562,13 +562,20 @@ public class EditorController implements Initializable {
                                 destination = new File(f.toPath().resolve(name).toString());
                                 break;
                             }
-                            
+                        }
+                        
+                        boolean noNewFileBrowserEntry = false;
+                        
+                        if (destination.exists() && !destination.isDirectory()) {
+                            noNewFileBrowserEntry = true;
                         }
                         
                         Files.copy(file.toPath(), destination.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                        FileTreeItem item = new FileTreeItem(new FileWrapper(destination));
-                        item.setGraphic(item.getIconNodeByFile(destination));
-                        fileBrowser.getSelectionModel().getSelectedItem().getParent().getChildren().add(item);
+                        if (!noNewFileBrowserEntry) {
+                            FileTreeItem item = new FileTreeItem(new FileWrapper(destination));
+                            item.setGraphic(item.getIconNodeByFile(destination));
+                            fileBrowser.getSelectionModel().getSelectedItem().getParent().getChildren().add(item);
+                        }
                     } catch (IOException e) {
                         Alert alert = new Alert(AlertType.ERROR);
                         alert.setTitle("ERROR");
